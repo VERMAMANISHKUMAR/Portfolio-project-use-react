df88a950714087f50c6e536acf7d875224498fdc
VERMAMANISHKUMAR <manishkumarverma091@gmail.com>
2024-05-11 12:28:31 +0530

    Work page is completed

--- src/Components/Portfolio/Portfolio.jsx
+++ src/Components/Portfolio/Portfolio.jsx
@@ -1,9 +1,31 @@
 import React from 'react'
 import './Portfolio.css'
+import {Swiper, SwiperSlide} from 'swiper/react'
+import 'swiper/css'
+
+
 const Portfolio = () => {
   return (
-    <div>
-      Portfolio
+    <div className='portfolio'>
+    {/* heading */}
+    <span>React Project</span><br/>
+    <span>Portfolio</span>
+    {/* Slider */}
+    <Swiper  spaceBetween={30} slidesPerView={3} grabCursor={true} className='portfolio-slider'>
+          <SwiperSlide>
+          <img src="img/sidebar.png" alt="icon is missing" />   
+          </SwiperSlide>
+          <SwiperSlide>
+          <img src="img/ecommerce.png" alt="icon is missing" />   
+          </SwiperSlide>
+          <SwiperSlide>
+          <img src="img/musicapp.png" alt="icon is missing" />   
+          </SwiperSlide>
+          <SwiperSlide>
+          <img src="img/hoc.png" alt="icon is missing" />   
+          </SwiperSlide>
+    </Swiper>
+     
     </div>
   )
 }
