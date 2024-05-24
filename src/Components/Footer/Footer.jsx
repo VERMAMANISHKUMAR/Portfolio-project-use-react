fa704ba3a1dd3a47cdf72f26941f881521a5ade5
VERMAMANISHKUMAR <manishkumarverma091@gmail.com>
2024-05-24 19:51:22 +0530

    footer page is completed and the page is finished

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -1,22 +1,25 @@
 import React from 'react'
 import './Footer.css'
 import Insta from '@iconscout/react-unicons/icons/uil-instagram'
-import Fessbook from '@iconscout/react-unicons/icons/uil-fessbook'
+import Fessbook from '@iconscout/react-unicons/icons/uil-facebook'
 import Github from '@iconscout/react-unicons/icons/uil-github'
 import Wave  from "../../image/wave.png"
 const Footer = () => {
   return (
-    <div className='footer'>
-      <img src={Wave} alt=' ' style={{width:'100%'}}/>
-      <div className='f-content'>
-      <span>Manish.sviet02@gmail.com</span>
-      <div className='f-icons'>
-        <Insta color='white' size='3rem'/>
-        <Fessbook color='white' size='3rem'/>
-        <Github color='white' size='3rem'/>
-      </div>
 
-      </div>
+    <div className='footer'>
+
+      <img src={Wave} alt=' ' style={{width:'100%'}}/>
+
+              <div className='f-container'>
+                     <span>Manish.sviet02@gmail.com</span>
+                    <div className='f-icons'>
+                             <Insta color='white' size='3rem'/>
+                             <Fessbook color='white' size='3rem'/>
+                             <Github color='white' size='3rem'/>
+                    </div>
+       
+              </div>
     </div>
   )
 }
